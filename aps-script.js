3d1a40acc3a6cc205995952c7d709cceec59cccd
Mike Ulvila <mikeulvila@me.com>
2015-10-20 10:08:31 -0500

    finished bonus content

--- aps-script.js
+++ aps-script.js
@@ -8,16 +8,36 @@ var clearMessage = document.getElementById("clear-message");
 var messageBoard = document.getElementById("messages");
 
 //***************CREATE ARRAY**************************
-
+var messageBox = [];
 //***************ENTER MESSAGE***********************
 document.getElementById("enter-message").addEventListener("keyup", function() {
 	if (event.keyCode === 13) {
 		if (editMode === false) {
-		var inputMessage = "<p><span class='only-text'>"
-		inputMessage += inputText.value
-		inputMessage += "</span><button class='edit'>Edit</button><button class='delete-button'>Delete</button>"
-		inputMessage += "</p>"
-		messageBoard.innerHTML += inputMessage;
+//*******************ADD DATE AND TIME*****************************
+		var date = new Date();
+		var year = date.getFullYear();
+		var month = date.getMonth() + 1;
+		var day = date.getDate();
+		var hours = date.getHours();
+		var minutes = date.getMinutes();
+		var printDate = month + "/" + day + "/" + year + " at " + hours + ":" + minutes;
+		console.log(printDate);
+//*******************ADD DATE AND TIME*****************************
+		var inputMessage = "<div><span class='only-text'>";
+		inputMessage += inputText.value;
+		inputMessage += "</span><button class='edit'>Edit</button><button class='delete-button'>Delete</button>";
+		inputMessage += printDate + "</div>";
+//*******************UNSHIFT TO ARRAY MESSAGEBOX*****************************
+		if (messageBox.length <= 19) {
+		messageBox.unshift(inputMessage);
+		} else if (messageBox.length >= 20) {
+			messageBox.unshift(inputMessage);
+			messageBox.pop();
+		} 
+		console.log("messageBox", messageBox);
+		console.log("length", messageBox.length);
+//*******************INSERT MESSAGE BOX TO HTML*****************************
+		messageBoard.innerHTML =  messageBox.join(" ");
 		inputText.value = "";
 	    } else if (editMode === true) {
 	    	editButtonSibling.innerHTML = inputText.value;
@@ -66,6 +86,9 @@ document.querySelector("body").addEventListener("click", function(event) {
   	editMode = true;
   }
 })
+
+
+
   	//document.getElementById("enter-message").addEventListener("keyup", function() {
 	// if (event.keyCode === 13) {
 	// 	event.target.previousSibling.innerHTML = inputText.value;
